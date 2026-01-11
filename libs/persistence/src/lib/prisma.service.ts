d21912434ffe54cbb08eb664f3036d02ea5a6968
Vladyslav Bilotskyi <hypeshithype@gmail.com>
2026-01-11 16:03:24 +0200

    persisted data in database

--- libs/persistence/src/lib/prisma.service.ts
+++ libs/persistence/src/lib/prisma.service.ts
@@ -1,11 +1,20 @@
+import 'dotenv/config';
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@my-project/db-client';
+
+const connectionString = process.env.DATABASE_URL ?? '';
+const adapter = new PrismaPg({ connectionString });
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    super({ adapter });
+  }
+
   async onModuleInit(): Promise<void> {
     await this.$connect();
   }
